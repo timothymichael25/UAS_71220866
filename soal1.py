d19715288980dea4ee067ed7e98a4bbc175d5b83
timothymichael25 <117884601+timothymichael25@users.noreply.github.com>
2023-01-03 12:23:47 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,19 @@
+print("****** Kredit Keaktifan Mahasiswa *******")
+print(" (Student Activities Credit) ")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Point")
+print("3. Keluar")
+print("------------------------------------------")
+(input("Silahkan Masukan Pilihan Anda: "))
+if '1':
+    print(input("Nama Kegiatan: "))
+    (input("Tanggal Kegiatan: "))
+    print("Pilihan Kategori: ")
+    print("- Prestasi")
+    print("- Organisasi")
+    print("- Kepanitian")
+    print("- Rekognisi")
+    (input("Masukan Kategori Kegiatan: "))
+    print("Kegiatan berhasil ditambahkan.")
+if '2':
+    print 
